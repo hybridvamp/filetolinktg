2e3ecd6acfa142a45960f6b33d09be11fe5c8848
Zainul Abid <48980248+zainulhibath@users.noreply.github.com>
2021-11-13 13:00:58 +0400

    Update __init__.py

--- bot/__init__.py
+++ bot/__init__.py
@@ -49,7 +49,7 @@ async def download(event):
                 caption=f"@{sender.username}|[{event.sender_id}](tg://user?id={event.sender_id})/{event.message.id}")
             id_hex = hex(msg.id)[2:]
             id = f"{id_hex}/{get_file_name(msg)}"
-            bot_url = f"t.me/{username_bot}?start={id_hex}"
+            bot_url = f"https://t.me/{username_bot}?start={id_hex}"
             await event.reply(f"Link to download file: \n\n📎 : {Config.DOMAIN}/{id}\n\n🤖 : {bot_url}")
             return
 
@@ -70,7 +70,7 @@ async def download(event):
                             return await event.reply("404! File Not Found")
                         forward = await file.forward_to(event.chat_id)
                         id_name = f"{id_hex}/{get_file_name(msg)}"
-                        bot_url = f"t.me/{username_bot}?start={id_hex}"
+                        bot_url = f"https://t.me/{username_bot}?start={id_hex}"
                         forward_reply = await forward.reply(f"will be deleted in 21 seconds. \n\n📎 : {Config.DOMAIN}/{id_name}\n\n🤖 : {bot_url}",link_preview=False)
                         await asyncio.sleep(12)
                         await forward_reply.edit(f"will be deleted in 10 seconds. \n\n📎 : {Config.DOMAIN}/{id_name}\n\n🤖 : {bot_url}")
